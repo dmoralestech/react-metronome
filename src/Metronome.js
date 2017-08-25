f759529ba68ebe488b793708d7cb60d0abe0ca67
darwinmorales <darwin.morales.tech@gmail.com>
2017-08-26 06:53:06 +1000

    add Metronome

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,10 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Metronome.css'
 
 const Metronome = () => {
+    let bpm = 100;
+    let playing = false;
+
     return (
         <div className="metronome">
-            hi
+            <div className="bpm-slider">
+                <div>{bpm} BPM</div>
+                <input
+                    type="range"
+                    min="60"
+                    max="240"
+                    value={bpm} />
+            </div>
+            <button>
+                {playing ? 'Stop' : 'Start'}
+            </button>
         </div>
     );
 };
