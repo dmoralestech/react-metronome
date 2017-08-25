0346916ddc48a4dff7d9a036bb2b9c484307d16b
darwinmorales <darwin.morales.tech@gmail.com>
2017-08-26 06:44:54 +1000

    add Metronome

---
+++ src/Metronome.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Metronome.css'
+
+const Metronome = () => {
+    return (
+        <div className="metronome">
+            hi
+        </div>
+    );
+};
+
+export default Metronome;
