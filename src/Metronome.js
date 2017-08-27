7eb549ee0d326c30c9b5e9d779af2ab00198f8aa
darwinmorales <darwin.morales.tech@gmail.com>
2017-08-27 20:26:15 +1000

    add Metronome - add sounds

--- src/Metronome.js
+++ src/Metronome.js
@@ -23,6 +23,40 @@ class Metronome extends Component {
         this.setState({bpm});
     }
 
+    startStop = () => {
+        if(this.state.playing) {
+            // Stop the timer
+            clearInterval(this.timer);
+            this.setState({
+                playing: false
+            });
+        } else {
+            // Start a timer with the current BPM
+            this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+            this.setState({
+                count: 0,
+                playing: true
+                // Play a click "immediately" (after setState finishes)
+            }, this.playClick);
+        }
+    }
+
+    playClick = () => {
+        const { count, beatsPerMeasure } = this.state;
+
+        // The first beat will have a different sound than the others
+        if(count % beatsPerMeasure === 0) {
+            this.click2.play();
+        } else {
+            this.click1.play();
+        }
+
+        // Keep track of which beat we're on
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerMeasure
+        }));
+    }
+
     render() {
         const {playing, bpm} = this.state;
 
@@ -35,10 +69,10 @@ class Metronome extends Component {
                         min="60"
                         max="240"
                         value={bpm}
-                        onChange={this.handleBpmChange}
-                    />
+                        onChange={this.handleBpmChange} />
                 </div>
-                <button>
+                {/* Add the onClick handler: */}
+                <button onClick={this.startStop}>
                     {playing ? 'Stop' : 'Start'}
                 </button>
             </div>
