591c9973862425907f6d85390c97d95a1f11633d
darwinmorales <darwin.morales.tech@gmail.com>
2017-10-25 22:45:20 +1100

    playing with Ramda

--- src/index.js
+++ src/index.js
@@ -7,14 +7,37 @@
 // ReactDOM.render(<Metronome />, document.getElementById('root'));
 // registerServiceWorker();
 
-import React from 'react'
-import { render } from 'react-dom'
-import { compose, map, prop } from 'ramda'
+import React from 'react';
+import { render } from 'react-dom';
+import { compose, map, prop, curry, reduce, pipe } from 'ramda';
 
-const List = items => <ul>{items}</ul>
-const Item = todo => <li key={todo.id}>{todo.text}</li>
-const getTodos = prop('todos')
-const TodoList = compose(List, map(Item), getTodos)
-const props = {todos: [{id: 1, text: 'foo'}, {id: 2, text: 'bar'}, {id: 3, text: 'baz'}]}
+const combine = curry((c, o) => x => (
+  <div>
+    {c(x)} {o(x)}
+  </div>
+));
 
-render(<TodoList {...props} />, document.getElementById('root'))
+const combineComponents = (...args) => {
+  const [first, ...rest] = args;
+  return reduce((acc, c) => combine(acc, c), first, rest);
+};
+
+const state = {
+  year: '2016',
+  title: 'Random stuff',
+  todos: [{ id: 1, text: 'foo' }, { id: 2, text: 'bar' }],
+};
+
+const getTodos = prop('todos');
+
+const Header = title => <h1>A Todo List: {title}</h1>;
+const List = items => <ul>{items}</ul>;
+const Item = todo => <li key={todo.id}>{todo.text}</li>;
+const Footer = text => <div>{text}</div>;
+
+const TodoHeader = pipe(s => s.title, Header);
+const TodoList = pipe(getTodos, compose(List, map(Item)));
+const TodoFooter = pipe(s => s.year, Footer);
+
+const App = combineComponents(TodoList, TodoHeader, TodoFooter);
+const result = render(<App {...state} />, document.getElementById('root'));
